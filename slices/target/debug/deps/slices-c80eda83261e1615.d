4b19b2a0f2aaf6f4d476f2709c3dcbde7c2721f7
Mohamed Ashour <m.aly.ashour@gmail.com>
2023-05-18 18:35:17 +0200

    adding slices

--- slices/target/debug/deps/slices-c80eda83261e1615.d
+++
@@ -1,5 +0,0 @@
-/Users/mohamedashour/workspace/learning_rust/slices/target/debug/deps/slices-c80eda83261e1615: src/main.rs
-
-/Users/mohamedashour/workspace/learning_rust/slices/target/debug/deps/slices-c80eda83261e1615.d: src/main.rs
-
-src/main.rs:
--- slices/target/debug/slices.d
+++
@@ -1,1 +0,0 @@
-/Users/mohamedashour/workspace/learning_rust/slices/target/debug/slices: /Users/mohamedashour/workspace/learning_rust/slices/src/main.rs
