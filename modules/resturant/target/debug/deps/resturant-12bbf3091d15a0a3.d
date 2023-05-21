f6ad4d806f1223e121c9ee3cea4325c7f7b9b784
Mohamed Ashour <m.aly.ashour@gmail.com>
2023-05-21 09:33:24 +0200

    Adding modules

--- modules/resturant/target/debug/deps/resturant-12bbf3091d15a0a3.d
+++
@@ -1,9 +0,0 @@
-/Users/mohamedashour/workspace/learning_rust/modules/resturant/target/debug/deps/resturant-12bbf3091d15a0a3.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/Users/mohamedashour/workspace/learning_rust/modules/resturant/target/debug/deps/libresturant-12bbf3091d15a0a3.rlib: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/Users/mohamedashour/workspace/learning_rust/modules/resturant/target/debug/deps/resturant-12bbf3091d15a0a3.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-src/lib.rs:
-src/front_of_house.rs:
-src/front_of_house/hosting.rs:
--- modules/resturant/target/debug/libresturant.d
+++
@@ -1,1 +0,0 @@
-/Users/mohamedashour/workspace/learning_rust/modules/resturant/target/debug/libresturant.rlib: /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/front_of_house/hosting.rs /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/front_of_house.rs /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/lib.rs
