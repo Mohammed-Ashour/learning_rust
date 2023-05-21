3bb4c13141a2a2071afa902566a5427eec5f1077
Mohamed Ashour <m.aly.ashour@gmail.com>
2023-05-21 09:29:29 +0200

    Adding modules

---
+++ modules/resturant/target/debug/libresturant.d
@@ -0,0 +1,1 @@
+/Users/mohamedashour/workspace/learning_rust/modules/resturant/target/debug/libresturant.rlib: /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/front_of_house/hosting.rs /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/front_of_house.rs /Users/mohamedashour/workspace/learning_rust/modules/resturant/src/lib.rs
