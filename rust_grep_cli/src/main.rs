9af4ff8b11b3b6827749b563c8d68899fae7d803
Mohamed Ashour <m.aly.ashour@gmail.com>
2023-06-04 19:38:49 +0200

    adding error println

--- rust_grep_cli/src/main.rs
+++ rust_grep_cli/src/main.rs
@@ -5,13 +5,13 @@ use args::Args;
 use file_op::File;
 fn main() {
     let args= Args::parse_args().unwrap_or_else(|err|{
-        println!("Problem with parsing arguments : {}", err);
+        eprintln!("Problem with parsing arguments : {}", err);
         process::exit(1);
     });
     let mut f = File::default();
     
     if let Err(e) = f.read_file(args.filepath){
-        println!("Error during reading the file");
+        eprintln!("Error during reading the file");
         process::exit(1)
     };
     println!("{}", f.content);
